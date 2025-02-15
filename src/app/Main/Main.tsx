4761722f761df9dbdb99fb527a587e0295317bf5
AslamtoIbrahim <aslamtoibrahim@gmail.com>
2025-02-15 17:04:56 +0100

    FIX adjusted background for large screens

--- src/app/Main/Main.tsx
+++ src/app/Main/Main.tsx
@@ -4,7 +4,8 @@ import Storage from './Storage'
 
 const Main = () => {
   return (
-    <div className='h-screen bg-mobile md:bg-desktop bg-no-repeat bg-center bg-cover 
+    <div className='h-screen bg-mobile md:bg-desktop bg-no-repeat bg-center bg-cover
+     md:bg-bottom  md:bg-contain md:bg-neutral-veryDarkBlue
     flex flex-col lg:flex-row items-center justify-center gap-4
     lg:px-20 xl:px-72'>
       <Fylo />
